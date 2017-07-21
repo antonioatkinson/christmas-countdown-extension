29929a972cfb35cb746b75a89048f304f8cdb254
antonioatkinson <antonioatkinson123@gmail.com>
2017-07-21 16:05:00 -0400

    Countdown clock UI changes

--- popup.js
+++ popup.js
@@ -13,11 +13,16 @@ function CalcChristmas(){
       		var minutesRemaining = 59+(christmas.getMinutes()-currentDate.getMinutes());
       		var secondsRemaining = 59+(christmas.getSeconds()-currentDate.getSeconds());
 
+      		$("#seconds").slideDown("fast",function(){
+      			document.getElementById("seconds").innerHTML = secondsRemaining;
+      		});
+
       		document.getElementById("months").innerHTML = monthsRemaining;
       		document.getElementById("days").innerHTML = daysRemaining;
       		document.getElementById("hours").innerHTML = hoursRemaining;
       		document.getElementById("minutes").innerHTML = minutesRemaining;
-      		document.getElementById("seconds").innerHTML = secondsRemaining;
+      		//document.getElementById("seconds").innerHTML = secondsRemaining;
+
       		//document.getElementById("countdown").innerHTML = ("There are "+monthsRemaining+ " months "+daysRemaining+" days "+hoursRemaining+" hours "+minutesRemaining+" minutes "+secondsRemaining+" seconds until Christmas Day");
       		//Refreshes function every second
       		setTimeout(CalcChristmas, 1000);
